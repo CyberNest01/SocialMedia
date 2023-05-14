211a00976917d0fbe4cb4d5c93b8b506c115c191
hosein <b.mohammadi.m@gmail.com>
2023-05-14 15:37:23 +0330

    add group in chats models ...

--- SocialMedia/chat/models.py
+++ SocialMedia/chat/models.py
@@ -17,8 +17,24 @@ class Chats(models.Model):
         super(Chats, self).save()
 
 
+class Group(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="group_owner")
+    users = models.ManyToManyField(User, related_name="group_users", null=True, blank=True)
+    name = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    bio = models.TextField()
+    deleted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        super(Group, self).save()
+
+
 class Message(models.Model):
-    chats = models.ForeignKey(Chats, on_delete=models.CASCADE)
+    chats = models.ForeignKey(Chats, on_delete=models.CASCADE, null=True, blank=True, related_name="message_chats")
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True, related_name="message_group")
     text = models.TextField()
     deleted = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)

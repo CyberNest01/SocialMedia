c64117c154bc50451670c209df2a72ebb7b050f5
hosein <b.mohammadi.m@gmail.com>
2023-06-03 14:00:58 +0330

    some edit ...

--- SocialMedia/post/api/like/like_post.py
+++ SocialMedia/post/api/like/like_post.py
@@ -1,14 +1,4 @@
-from django.db.models import Q
-from drf_yasg.utils import swagger_auto_schema
-from rest_framework.permissions import *
-from rest_framework.response import Response
-from rest_framework.status import *
-from rest_framework.views import APIView
-
 from .imports import *
-from ..dto import *
-from post.models import *
-from ..serializers import LikeSerializer
 
 
 class LikePost(APIView):
